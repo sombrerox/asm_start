2185992930f5dbd56fb1cf8f9b7b20d0c1eb1374
Edin <medin932@gmail.com>
2021-01-04 23:41:47 +0100

    Update greatest_element_in_the_list.asm

--- greatest_element_in_the_list.asm
+++ greatest_element_in_the_list.asm
@@ -11,20 +11,21 @@ list_elements:
 	
     .globl _start
 _start:
-    movl $0, %edi # we set index of the highest element to 0
-    movl list_elements(,%edi,4), %eax # set first element in eax
-    movl %eax, %ebx # initially set value loaded in eax to be the largest (load it in ebx)
+    movl $0, %edi #we set index of the highest element to 0
+    movl list_elements(,%edi,4), %eax #set first element in eax
+    movl %eax, %ebx #initially set value loaded in eax to be the largest (load it in ebx)
     
 start_check:
-    cmpl $0, %eax # compare current value in eax with 0 (so we know if we reached end)
-    je end_check
-    incl %edi
-    movl list_elements(,%edi,4), %eax
-    cmpl %ebx, %eax
-    jle start_check
-    movl %eax, %ebx
-    jmp start_check
+    cmpl $0, %eax #compare current value in eax with 0 (so we know if we reached end)
+    je end_check #if current value is equal to 0 we have reached the end of the list, end the loop
+    incl %edi #increment index
+    movl list_elements(,%edi,4), %eax #set eax to next number
+    cmpl %ebx, %eax #compare current number with greatest we have found so far 
+    jle start_check #if current number is less than or equal to the greatest we have found until now, go back to start of the loop
+    movl %eax, %ebx #otherwise if it's greater move it to ebx
+    jmp start_check #unconditionally jump to start of the loop
     
 end_check:
-    movl $1, %eax
-    int $0x80
+    movl $1, %eax #move 1 to eax (1 representing exit code, it is read from eax register when system exit call is made,
+    	          #value of ebx is return status code, which will in our case be value of the greatest number in the list)
+    int $0x80 #make a system call
